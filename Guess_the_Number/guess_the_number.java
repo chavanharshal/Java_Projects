4155760b532ae6517ba8cfc8d1885150042081e8
chavanharshal <109310459+chavanharshal@users.noreply.github.com>
2024-10-20 23:19:57 +0530

    Create guess_the_number.java

---
+++ Guess_the_Number/guess_the_number.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        // Generate a random number between 1 and 100
+        int numberToGuess = random.nextInt(100) + 1;
+        int numberOfTries = 0;
+        boolean hasGuessedCorrectly = false;
+
+        System.out.println("Welcome to 'Guess the Number'!");
+        System.out.println("I'm thinking of a number between 1 and 100.");
+
+        // Game loop
+        while (!hasGuessedCorrectly) {
+            System.out.print("Enter your guess: ");
+            int playerGuess = scanner.nextInt();
+            numberOfTries++;
+
+            if (playerGuess < 1 || playerGuess > 100) {
+                System.out.println("Please guess a number between 1 and 100.");
+            } else if (playerGuess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (playerGuess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                hasGuessedCorrectly = true;
+                System.out.println("Congratulations! You've guessed the number " + numberToGuess + " in " + numberOfTries + " tries.");
+            }
+        }
+
+        scanner.close();
+    }
+}
